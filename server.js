06470a49654528bc995de6ec99ce9350d310ec38
Sivaranjani282 <Sivaranjani282@users.noreply.github.com>
2017-08-31 06:12:17 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.get('/article-one', function (req, res) {
 app.get('/article-two', function (req, res) {
   res.send('This is Article Two');
 });
-app.get('/', function (req, res) {
+app.get('/article-three', function (req, res) {
   res.send('This is Article Three');
 });
 
